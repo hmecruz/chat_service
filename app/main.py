e26a3b7d5f17758a1ff26a558901887d7c7b9a7a
Henrique Cruz <hmecruz@gmail.com>
2025-05-15 15:35:51 +0100

    containerization. Unstable version missing fixes for the flask app

--- app/main.py
+++ app/main.py
@@ -6,9 +6,10 @@ if __name__ == '__main__':
     # Start the Flask-SocketIO server.
     socketio.run(
         app,
-        host=app.config['HOST'],
-        port=int(app.config['PORT']),
-        debug=True
+        host=app.config['FLASK_HOST'],
+        port=int(app.config['FLASK_PORT']),
+        debug=True,
+        allow_unsafe_werkzeug=True # For development only, not recommended for production (use eventlet instead). 
     )
 
 # run: python -m app.main
--- config/service_config.py
+++ config/service_config.py
@@ -2,7 +2,7 @@ from .base_config import get_env_variable
 
 class ServiceConfig:
     """Service-related configurations."""
+    FLASK_HOST = get_env_variable("FLASK_HOST", "localhost")
+    FLASK_PORT = get_env_variable("FLASK_PORT", 5000)
     API_KEY = get_env_variable("API_KEY", "default_api_key")
-    HOST = get_env_variable("HOST", "localhost")
-    PORT = get_env_variable("PORT", 5000)
     JWT_SECRET_KEY = get_env_variable("JWT_SECRET_KEY")
